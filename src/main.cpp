b97ff931ced4b31ee14469fe38d27f6bfddfc442
Piyush Kumar <piyushkumg@gmail.com>
2021-06-15 20:21:05 +0530

    Add line editing and command history
    Using the linenoise library from
    https://github.com/yhirose/cpp-linenoise
    
    Will make CMakeList nicer, later

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 
 #include "../include/mal.hpp"
+#include "../lib/linenoise.hpp"
+
+constexpr auto HISTORY_PATH = "malpp_history";
+constexpr auto PROMPT       = "user> ";
 
 int main()
 {
         while (true)
         {
-                std::cout << "user> ";
-
+                linenoise::LoadHistory(HISTORY_PATH);
                 std::string input;
-                std::getline(std::cin, input);
 
-                if (!std::cin)
-                        return 1;
+                auto quit = linenoise::Readline(PROMPT, input);
+
+                if (quit)
+                        break;
 
                 std::cout << rep(input) << "\n";
+
+                linenoise::AddHistory(input.c_str());
         }
+
+        linenoise::SaveHistory(HISTORY_PATH);
 
         return 0;
 }
